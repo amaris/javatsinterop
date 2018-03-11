7bd82b5bc5e78e475bfff392de192df5c4b2811e
Renaud Pawlak <Renaud Pawlak@DESKTOP-D2K3CKT>
2018-03-11 21:57:47 +0100

    use XHR + change of license to MIT

--- examples/www/js/bubbletreeController.ts
+++ examples/www/js/bubbletreeController.ts
@@ -1,10 +1,35 @@
-
+/* 
+ * MIT License
+ * 
+ * Copyright (c) 2018 Amaris <rpawlak@amaris.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 import GenericNode = com.amaris.javatsinterop.dto.GenericNode;
+import TestResource = com.amaris.javatsinterop.api.TestResource;
 
 class BubbleTreeController {
+
     constructor() {
-        let tree : BubbleTree<GenericNode> = new BubbleTree<GenericNode>();
+        let tree: BubbleTree<GenericNode> = new BubbleTree<GenericNode>();
+        let test: TestResource = new TestResource("/");
         tree.build({
             container: document.getElementById("bubbletree"),
             url: "/home/tree",
@@ -12,10 +37,9 @@ class BubbleTreeController {
             handlers: {
                 "click": function(d) {
                     console.info(">> " + d.data.weight);
-                    console.info(">> " + d.data.weight);
-                    
+                    test.helloWorld((s) => { console.info("ASYNC >> " + s); })
                 },
-                "mouseover": function(d) { console.info("mouseover: "+d.data.test); },
+                "mouseover": function(d) { console.info("mouseover: " + d.data.test); },
                 "dblclick": function(d) { console.info("dblclick"); }
             },
             showRoot: false
--- examples/www/js/generated/stubs.ts
+++ examples/www/js/generated/stubs.ts
@@ -1,21 +1,27 @@
 namespace com.amaris.javatsinterop.api {
-  class TestResource {
+    export class TestResource {
 
-    constructor(private baseUrl : string) {}
+        private _xhr(method: string, url: string, callback: any): void {
+            var xhr = new XMLHttpRequest();
+            xhr.open(method, url, true);
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.onload = () => {
+                callback(JSON.parse(xhr.responseText));
+            };
+            xhr.send();
+        }
 
-    helloWorld(onSuccess : (data : string) => void) : void {
-      $.ajax({
-        url: this.baseUrl + "home/hello"
-      }).then(onSuccess);
+        constructor(private baseUrl: string) { }
+
+        helloWorld(callback: (data: string) => void): void {
+            this._xhr("GET", this.baseUrl + "home/hello", callback);
+        }
+
+        tree(callback: (data: com.amaris.javatsinterop.dto.GenericNode) => void): void {
+            this._xhr("GET", this.baseUrl + "home/tree", callback);
+        }
+
     }
-
-    tree(onSuccess : (data : com.amaris.javatsinterop.dto.GenericNode) => void) : void {
-      $.ajax({
-        url: this.baseUrl + "home/tree"
-      }).then(onSuccess);
-    }
-
-  }
 
 }
 
