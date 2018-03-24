dbd2cd05c5a6de397b5f13891a72dc4dccf0221a
Renaud Pawlak <Renaud Pawlak@DESKTOP-D2K3CKT>
2018-03-24 21:38:45 +0100

    javadoc

--- examples/src/main/java/com/amaris/javatsinterop/data/Data.java
+++ examples/src/main/java/com/amaris/javatsinterop/data/Data.java
@@ -1,3 +1,26 @@
+/* 
+ * MIT License
+ * 
+ * Copyright (c) 2018 Amaris <rpawlak@amaris.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.amaris.javatsinterop.data;
 
 import java.io.File;
@@ -10,6 +33,11 @@ import org.apache.commons.lang3.math.NumberUtils;
 import com.amaris.javatsinterop.dto.Salary;
 import com.opencsv.CSVReader;
 
+/**
+ * This class implements the data layer of this example.
+ * 
+ * @author Renaud Pawlak
+ */
 public class Data {
 
 	private List<Salary> salaries;
@@ -36,6 +64,9 @@ public class Data {
 		}
 	}
 
+	/**
+	 * Returns a list of salaries read from the salaries.csv file.
+	 */
 	public List<Salary> getSalaries() {
 		return salaries;
 	}
